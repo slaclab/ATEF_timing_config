32b728e57c831495b1bb344c8780c3292b9c7dbc
Rizheng Jiang <rj1@slac.stanford.edu>
2024-02-01 17:45:33 -0800

    Changes made for pull requests

--- intra_hutch_main.py
+++ intra_hutch_main.py
@@ -17,11 +17,22 @@ import config
 from datetime import datetime
 import json
 import os
+import argparse
+from argparse import RawTextHelpFormatter
+
+parser = argparse.ArgumentParser(description='intra hutch usage:', formatter_class=RawTextHelpFormatter)
+parser.add_argument("hutch_code", help="Enter the code of the hutch you want to check \n\nHutch code list: \nCXI_FS5: 5  \nMEC_FS6: 6  \nMFX_FS4.5: 45  \nNEH_FS11: 11 \nNEH_FS14: 14  \nXCS_FS4: 4\n\n", type=str)
+parser.add_argument("total_iterations", help="Enter total iterations of random sampling (Default: 5)\n", nargs='?', const=1, type=int, default=5)
+parser.add_argument("interval", help="Enter max interval of each iteration in seconds (Default: 5) \n", nargs='?', const=1, type=float, default=5)
 
 
-# Gather user inputs for comparision and sifting
-print("Hutch code list: \n\nCXI_FS5: 5  \nMEC_FS6: 6  \nMFX_FS4.5: 45  \nNEH_FS11: 11 \nNEH_FS14: 14  \nXCS_FS4: 4\n")
-hutch_code = input("Enter hutch code: ")
+args = parser.parse_args()
+
+
+### Gather user inputs for comparision and sifting
+
+# print("Hutch code list: \n\nCXI_FS5: 5  \nMEC_FS6: 6  \nMFX_FS4.5: 45  \nNEH_FS11: 11 \nNEH_FS14: 14  \nXCS_FS4: 4\n")
+# hutch_code = input("Enter hutch code: ")
 hutch_dict = {
 "5" : "CXI_FS5", 
 "6" : "MEC_FS6",  
@@ -30,7 +41,7 @@ hutch_dict = {
 "14" : "NEH_FS14",  
 "4" : "XCS_FS4"}
 
-hutch = hutch_dict[hutch_code]
+hutch = hutch_dict[args.hutch_code]
 folder= os.getcwd() + "/" + hutch + "/"
 ioc_path = folder + hutch + '_IOC.txt'
 
@@ -39,16 +50,16 @@ pv_lists_path = folder + "temp/" + "pv_lists/"
 
 subprocess.run(["mkdir", "-p", pv_lists_path])
 
-total_iterations = int(input("Enter total iterations of random sampling: "))
-interval = float(input("Enter max interval of each iteration in seconds: "))
+# total_iterations = int(input("Enter total iterations of random sampling: "))
+# interval = float(input("Enter max interval of each iteration in seconds: "))
 
 
 
 
-# Run through all the IOCs of a hutch and configure the JSON file.
+### Run through all the IOCs of a hutch and configure the JSON file.
 with open(ioc_path, 'r') as readtxt:
     ioc_list = [line.strip() for line in readtxt.readlines()]
-    print("Estimated wait time: " + str(int(len(ioc_list) * total_iterations * (interval/2) / 60 + 2)) + " minutes.")
+    print("Estimated wait time: " + str(int(len(ioc_list) * args.total_iterations * (args.interval/2) / 60 + 2)) + " minutes.")
     for ioc in ioc_list:
         #print("Getting .pvlist file from " + ioc)
         subprocess.run(["cp", "/cds/data/iocData/"+ ioc +"/iocInfo/IOC.pvlist", pv_lists_path + ioc +"_IOC.pvlist"])
@@ -60,28 +71,28 @@ with open(ioc_path, 'r') as readtxt:
         folder, 
         pv_lists_path + ioc +"_IOC.pvlist", 
         pv_lists_path + "new_" + ioc +"_IOC.pvlist", 
-        total_iterations, interval
+        args.total_iterations, args.interval
         )
 
 
 
 
-#Getting date infomation
+### Getting date infomation
 now = datetime.now()
 date = now.strftime("%Y%m%d")
 date_time = now.strftime("%m/%d/%Y, %H:%M:%S")
 
 
-#Add metadata and cleanup       
+### Add metadata and cleanup       
 with open(folder + date + hutch + ".json", 'r') as openfile:
     # Reading from json file
     config_template = json.load(openfile)   
 
-#Adding hutch name in JSON    
+### Adding hutch name in JSON    
 if config_template["root"]["name"] != hutch:
     config_template["root"]["name"] = hutch
     
-#Adding timestamp in JSON
+### Adding timestamp in JSON
 config_template["root"]["description"] = date_time
 
 del config_template["root"]["configs"][0]
